8c9dfa016bfddc2f4f4d888c24d128afe044e7e3
bema97 <ibermet@gmail.com>
2019-07-08 12:24:17 +0600

    blog

---
+++ blog_poject/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    # path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('blog/', include('blog.urls'))
+
+]
